250b8d570b30211e862988ca3feab2c6c3aa7049
Haikel Fazzani <haikel.fazzanii@outlook.fr>
2017-11-30 13:31:02 +0100

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,31 @@
+$(function(){
+	$("#msg").hide();
+	var min = 1;
+	var max = 9;
+
+	function randNumber(min,max){
+		return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
+	var numRand = randNumber(min,max);
+
+	$("#guess").click(function(){
+	
+		var userInput = $("#num-field").val();
+
+		if(userInput == numRand){
+
+			$("#msg").text("congrats you won ! "+ numRand).css("background","#8bc34a").show();
+
+		}else if(userInput != numRand){
+
+			$("#msg").text("wrong guess !").css("background","red").show();
+		}
+	});
+
+
+	$("#start").click(function(){
+		location.reload();
+	});
+
+});
